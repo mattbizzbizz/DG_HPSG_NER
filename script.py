7dd4ba12ef9668870286a5aa9d143bb819409340
Matthew Buzzell <matabuzz@login1.hsn0.cm.bigred200.uits.iu.edu>
2023-04-30 19:34:35 -0400

    Added files.

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ language = sys.argv[1]    ## getting language from commandline
 parser = sys.argv[2]      ## getting parser (DG/HPSG) from commandline
 
 from library.preprocessing import *
-from library.model import *
+#from library.model import *
 from library.parsing import *
 
 
@@ -18,13 +18,13 @@ transform.run()
 
 ## ------------ parsing ------------
 if parser == 'DG':
-    java_path = "/N/soft/sles15/java/jdk-17/bin/java"
-    jar_path = './stanford-corenlp-4.2.2/stanford-corenlp-4.2.2.jar'
-    models_jar_path = './stanford-corenlp-4.2.2-models-english.jar'
+    #java_path = "/N/soft/sles15/java/jdk-17/bin/java"
+    jar_path = '~/DG_HPSG_NER/stanford-corenlp-4.2.2/stanford-corenlp-4.2.2.jar'
+    models_jar_path = '~/DG_HPSG_NER/stanford-corenlp-4.2.2-models-english.jar'
     special_tokens = ['[START]','[SEP]', '[TAG]', '[END]', '[UNK]', '[PAD]']
     max_len = 150
 
-    dg = DependencyGrammar(java_path, jar_path, models_jar_path, path, language, special_tokens, max_len, parser)
+    dg = DependencyGrammar(jar_path, models_jar_path, path, language, special_tokens, max_len, parser)
     dg.run()
     
 elif parser == 'HPSG':
@@ -36,13 +36,13 @@ elif parser == 'HPSG':
     hpsg.run()
     
 ## ------------ RNN model ------------
-if parser == 'DG':
-    max_len = 150
-else:
-    max_len = 100
-
-epochs = 3
-batch_size = 64
-
-mdl = RNN_Model(path, language, max_len, parser, batch_size, epochs)
-mdl.run()
+#if parser == 'DG':
+#    max_len = 150
+#else:
+#    max_len = 100
+#
+#epochs = 3
+#batch_size = 64
+#
+#mdl = RNN_Model(path, language, max_len, parser, batch_size, epochs)
+#mdl.run()
